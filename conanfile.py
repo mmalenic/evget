387fae25c1d4ddc3e2c2519b5b0e73c95ccfc68b
Marko Malenic <mmalenic1@gmail.com>
2021-07-22 16:08:32 +1000

    Allow downloading dependencies.

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,18 @@
-from conans import ConanFile
+from conans import ConanFile, CMake
 
 
 class App(ConanFile):
     settings = 'os', 'arch', 'compiler', 'build_type'
     requires = 'boost/1.76.0', 'spdlog/1.9.0'
     generators = 'cmake'
-    options = {'build_tests': [True, False]}
-    default_options = {'build_tests': True}
+    options = {
+        'build_tests': [True, False],
+        'download_dependencies': [True, False]
+    }
+    default_options = {
+        'build_tests': True,
+        'download_dependencies': True
+    }
 
     def requirements(self):
         if self.options.build_tests:
@@ -14,3 +20,14 @@ class App(ConanFile):
 
     def package_info(self):
         self.cpp_info.requires = ['boost::program_options', 'boost::algorithm']
+
+    def build(self):
+        cmake = CMake()
+
+        cmake.definitions['BUILD_TESTING'] = self.options.build_tests
+        cmake.definitions['DOWNLOAD_DEPENDENCIES'] = self.options.download_dependencies
+
+        if self.options.build_tests:
+            cmake.test()
+
+        cmake.build()
