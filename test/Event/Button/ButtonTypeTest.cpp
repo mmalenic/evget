0e09b05c67c8954d333d49f30dd5090e81fe6309
mmalenic <mmalenic1@gmail.com>
2022-02-14 16:34:05 +1100

    Add button type.

---
+++ test/Event/Button/ButtonTypeTest.cpp
@@ -0,0 +1,50 @@
+// MIT License
+//
+// Copyright (c) 2021 Marko Malenic
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+#include <gtest/gtest.h>
+#include "TestUtils/Event/EventTestUtils.h"
+#include "evget/Event/Button/ButtonType.h"
+
+namespace EventTestUtils = TestUtils::EventTestUtils;
+
+TEST(ButtonTypeTest, Constructor) { // NOLINT(cert-err58-cpp)
+    EventTestUtils::fieldValueAndName<Event::Button::ButtonType>("Button", "ButtonType", "Button");
+}
+
+TEST(ButtonTypeTest, Press) { // NOLINT(cert-err58-cpp)
+    auto type = Event::Button::ButtonType::createButtonType(Event::Button::ButtonAction::Press);
+    ASSERT_EQ("ButtonType", type->getName());
+    ASSERT_EQ("Press", type->getEntry());
+}
+
+TEST(ButtonTypeTest, Release) { // NOLINT(cert-err58-cpp)
+    auto type = Event::Button::ButtonType::createButtonType(Event::Button::ButtonAction::Release);
+    ASSERT_EQ("ButtonType", type->getName());
+    ASSERT_EQ("Release", type->getEntry());
+}
+
+TEST(ButtonTypeTest, Repeat) { // NOLINT(cert-err58-cpp)
+    auto type = Event::Button::ButtonType::createButtonType(Event::Button::ButtonAction::Repeat);
+    ASSERT_EQ("ButtonType", type->getName());
+    ASSERT_EQ("Repeat", type->getEntry());
+}
