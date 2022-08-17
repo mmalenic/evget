e77566c01f8f5bd1021615b2a8b9334295d57e5a
Marko Malenic <mmalenic1@gmail.com>
2022-08-17 20:58:14 +1000

    Add remaining field tests.

--- evgetcore/test/Event/FieldTest.cpp
+++ evgetcore/test/Event/FieldTest.cpp
@@ -65,8 +65,109 @@ TEST(FieldTest, IsData) { // NOLINT(cert-err58-cpp)
     ASSERT_FALSE(field.isEntry());
 }
 
-TEST(FieldTest, CreateAction) { // NOLINT(cert-err58-cpp)
+TEST(FieldTest, CreateActionPress) { // NOLINT(cert-err58-cpp)
     auto field = EvgetCore::Event::Field::createAction(EvgetCore::Event::ButtonAction::Press);
     ASSERT_EQ("Action", field.getName());
     ASSERT_EQ("Press", field.getEntry());
+}
+
+TEST(FieldTest, CreateActionRelease) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createAction(EvgetCore::Event::ButtonAction::Release);
+    ASSERT_EQ("Action", field.getName());
+    ASSERT_EQ("Release", field.getEntry());
+}
+
+TEST(FieldTest, CreateActionRepeat) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createAction(EvgetCore::Event::ButtonAction::Repeat);
+    ASSERT_EQ("Action", field.getName());
+    ASSERT_EQ("Repeat", field.getEntry());
+}
+
+TEST(FieldTest, CreateCharacter) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createCharacter("a");
+    ASSERT_EQ("Character", field.getName());
+    ASSERT_EQ("a", field.getEntry());
+}
+
+TEST(FieldTest, CreateIdentifier) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createIdentifier(1);
+    ASSERT_EQ("Identifier", field.getName());
+    ASSERT_EQ("1", field.getEntry());
+}
+
+TEST(FieldTest, CreateName) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createName("name");
+    ASSERT_EQ("Name", field.getName());
+    ASSERT_EQ("name", field.getEntry());
+}
+
+TEST(FieldTest, CreateDateTime) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createDateTime({});
+    ASSERT_EQ("DateTime", field.getName());
+}
+
+TEST(FieldTest, CreateDeviceTypeMouse) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createDeviceType(EvgetCore::Event::Device::Mouse);
+    ASSERT_EQ("Device", field.getName());
+    ASSERT_EQ("Mouse", field.getEntry());
+}
+
+TEST(FieldTest, CreateDeviceTypeKeyboard) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createDeviceType(EvgetCore::Event::Device::Keyboard);
+    ASSERT_EQ("Device", field.getName());
+    ASSERT_EQ("Keyboard", field.getEntry());
+}
+
+TEST(FieldTest, CreateDeviceTypeTouchpad) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createDeviceType(EvgetCore::Event::Device::Touchpad);
+    ASSERT_EQ("Device", field.getName());
+    ASSERT_EQ("Touchpad", field.getEntry());
+}
+
+TEST(FieldTest, CreateDeviceTypeTouchscreen) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createDeviceType(EvgetCore::Event::Device::Touchscreen);
+    ASSERT_EQ("Device", field.getName());
+    ASSERT_EQ("Touchscreen", field.getEntry());
+}
+
+TEST(FieldTest, CreateTime) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createTime(std::chrono::nanoseconds{1});
+    ASSERT_EQ("Time", field.getName());
+    ASSERT_EQ("1", field.getEntry());
+}
+
+TEST(FieldTest, CreatePositionX) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createPositionX(1);
+    ASSERT_EQ("PositionX", field.getName());
+    ASSERT_EQ("1", field.getEntry());
+}
+
+TEST(FieldTest, CreatePositionY) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createPositionY(1);
+    ASSERT_EQ("PositionY", field.getName());
+    ASSERT_EQ("1", field.getEntry());
+}
+
+TEST(FieldTest, CreateScrollUp) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createScroll(EvgetCore::Event::Direction::Up, 1);
+    ASSERT_EQ("ScrollUp", field.getName());
+    ASSERT_EQ("1", field.getEntry());
+}
+
+TEST(FieldTest, CreateScrollDown) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createScroll(EvgetCore::Event::Direction::Down, 1);
+    ASSERT_EQ("ScrollDown", field.getName());
+    ASSERT_EQ("1", field.getEntry());
+}
+
+TEST(FieldTest, CreateScrollLeft) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createScroll(EvgetCore::Event::Direction::Left, 1);
+    ASSERT_EQ("ScrollLeft", field.getName());
+    ASSERT_EQ("1", field.getEntry());
+}
+
+TEST(FieldTest, CreateScrollRight) { // NOLINT(cert-err58-cpp)
+    auto field = EvgetCore::Event::Field::createScroll(EvgetCore::Event::Direction::Right, 1);
+    ASSERT_EQ("ScrollRight", field.getName());
+    ASSERT_EQ("1", field.getEntry());
 }
