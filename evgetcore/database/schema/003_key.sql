5f7e2b1e2f4b272449135c819cd7a2bd95a15716
Marko Malenic <mmalenic1@gmail.com>
2023-11-20 14:46:48 +0100

    feat: add mouse click sql

---
+++ evgetcore/database/schema/003_key.sql
@@ -0,0 +1,26 @@
+-- A key table for key events.
+create table key (
+    id integer primary key,
+    interval real,
+    timestamp text not null,
+    device_type integer not null references device_type(id),
+    position_x real,
+    position_y real,
+    button_action integer not null references button_action(id),
+    button_id integer,
+    button_name text,
+    character text,
+    device_name text,
+    focus_window_name text,
+    focus_window_position_x real,
+    focus_window_position_y real,
+    focus_window_width real,
+    info text
+);
+
+-- Linking table to modifier
+create table key_modifier (
+    id integer primary key,
+    key_id integer not null references key(id),
+    modifier_id integer not null references modifier(id)
+);
--- evgetcore/database/schema/004_mouse_click.sql
+++ evgetcore/database/schema/004_mouse_click.sql
@@ -1,5 +1,5 @@
--- A key table for key events.
-create table key (
+-- A table for mouse click events.
+create table mouse_click (
     id integer primary key,
     interval real,
     timestamp text not null,
@@ -9,11 +9,17 @@ create table key (
     button_action integer not null references button_action(id),
     button_id integer,
     button_name text,
-    character text,
     device_name text,
     focus_window_name text,
     focus_window_position_x real,
     focus_window_position_y real,
     focus_window_width real,
     info text
+);
+
+-- Linking table to modifier
+create table mouse_click_modifier (
+    id integer primary key,
+    mouse_click_id integer not null references mouse_click(id),
+    modifier_id integer not null references modifier(id)
 );
