725e951abe4207a37aad6127aa75c7d0248c8ce4
Marko Malenic <mmalenic1@gmail.com>
2023-11-20 17:28:40 +0100

    refactor: directly use enum in modifier table

--- evgetcore/database/schema/001_types.sql
+++ evgetcore/database/schema/001_types.sql
@@ -1,21 +1,3 @@
--- Enum table representing possible modifier values.
-create table modifier_type (
-    id integer primary key,
-    enum text not null unique
-);
-insert into modifier_type values (1, 'Shift'),
-    -- X11 - Lock
-    (2, 'CapsLock'),
-    (3, 'Control'),
-    -- X11 - Mod1, Mac - Option
-    (4, 'Alt'),
-    -- X11 - Mod2
-    (5, 'NumLock'),
-    (6, 'Mod3'),
-    -- X11 - Mod4, Mac - Command, Windows - Windows key
-    (7, 'Super'),
-    (8, 'Mod5');
-
 -- Enum type representing button action types.
 create table button_action (
     id integer primary key,
--- evgetcore/database/schema/002_modifier.sql
+++ evgetcore/database/schema/002_modifier.sql
@@ -1,5 +1,17 @@
 -- A modifier table containing the type of modifier in effect.
 create table modifier (
     id integer primary key,
-    value integer not null references modifier_type(id)
+    value text not null unique
 );
+insert into modifier values (1, 'Shift'),
+    -- X11 - Lock
+    (2, 'CapsLock'),
+    (3, 'Control'),
+    -- X11 - Mod1, Mac - Option
+    (4, 'Alt'),
+    -- X11 - Mod2
+    (5, 'NumLock'),
+    (6, 'Mod3'),
+    -- X11 - Mod4, Mac - Command, Windows - Windows key
+    (7, 'Super'),
+    (8, 'Mod5');
