af85c03eab1399c7d6787b13ea14319d7dde43f9
Marko Malenic <mmalenic1@gmail.com>
2023-11-20 10:28:44 +0100

    refactor: use more universal table instead of enums

--- evgetcore/database/postgresql/schema/001_types.sql
+++ evgetcore/database/postgresql/schema/001_types.sql
@@ -1,41 +1,47 @@
-create schema evget;
-
--- Enum type representing possible modifier values.
-create type evget.modifier_type as enum (
-    'Shift',
-    -- X11 - Lock
-    'CapsLock',
-    'Control',
-    -- X11 - Mod1, Mac - Option
-    'Alt',
-    -- X11 - Mod2
-    'NumLock',
-    'Mod3',
-    -- X11 - Mod4, Mac - Command, Windows - Windows key
-    'Super',
-    'Mod5'
+-- Enum table representing possible modifier values.
+create table modifier_type (
+    id integer primary key,
+    enum text unique
 );
+insert into modifier_type values (1, 'Shift'),
+    -- X11 - Lock
+    (2, 'CapsLock'),
+    (3, 'Control'),
+    -- X11 - Mod1, Mac - Option
+    (4, 'Alt'),
+    -- X11 - Mod2
+    (5, 'NumLock'),
+    (6, 'Mod3'),
+    -- X11 - Mod4, Mac - Command, Windows - Windows key
+    (7, 'Super'),
+    (8, 'Mod5');
 
 -- Enum type representing button action types.
-create type evget.button_action as enum (
-    'Press',
-    'Release',
-    'Repeat'
+create table button_action (
+    id integer primary key,
+    enum text unique
 );
+insert into button_action values (1, 'Press'),
+    (2, 'Release'),
+    (3, 'Repeat');
 
 -- Enum type representing device types.
-create type evget.device_type as enum (
-    'Mouse',
-    'Keyboard',
-    'Touchpad',
-    'Touchscreen'
+create table device_type (
+    id integer primary key,
+    enum text unique
 );
+insert into device_type values (1, 'Mouse'),
+    (2, 'Keyboard'),
+    (3, 'Touchpad'),
+    (4, 'Touchscreen');
 
 -- Enum type representing scroll directions.
-create type evget.direction as enum (
-    'Down',
-    'Left',
-    'Right',
-    'Up'
+create table direction (
+    id integer primary key,
+    enum text unique
 );
+insert into direction values (1, 'Down'),
+    (2, 'Left'),
+    (3, 'Right'),
+    (4, 'Up');
 
