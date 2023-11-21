73e4a0b4c7674d68713efe771e95adb29673dc48
Marko Malenic <mmalenic1@gmail.com>
2023-11-21 17:56:18 +0100

    feat: add error types

---
+++ evgetcore/include/evgetcore/Storage/Error.h
@@ -0,0 +1,53 @@
+// MIT License
+//
+// Copyright (c) 2021 Marko Malenic
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#ifndef ERROR_H
+#define ERROR_H
+#include <exception>
+#include <string>
+#include <expected>
+
+namespace EvgetCore::Storage {
+    /**
+     * \brief Error type enum.
+     */
+    enum class ErrorType {
+        SQLiteError,
+    };
+
+    /**
+     * \brief Error struct.
+     */
+    struct Error {
+        ErrorType errorType;
+        std::string message;
+    };
+
+    /**
+     * \brief Result type.
+     */
+    template<typename T>
+    using Result = std::expected<T, Error>;
+}
+
+#endif //ERROR_H
