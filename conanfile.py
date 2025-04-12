c67a083a6abc09698dcfb67ab38a8fd929b24307
Marko Malenic <mmalenic1@gmail.com>
2025-04-13 09:39:41 +1000

    refactor: configure conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -19,36 +19,81 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-from conan.tools.cmake import cmake_layout
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import cmake_layout, CMakeToolchain, CMake
 from conan import ConanFile
 
 
-class App(ConanFile):
+
+class EvgetRecipe(ConanFile):
     name = 'evget'
-    version = '0.1'
-    description = 'Shows events from input devices.'
-    url = 'https://github.com/mmalenic/evget.git'
+    description = 'record input device events'
+    url = 'https://github.com/mmalenic/evget'
     license = 'MIT'
-    author = 'Marko Malenic'
-    topics = 'ev', 'evdev', 'events'
-    settings = 'os', 'arch', 'compiler', 'build_type'
-    requires = 'boost/[^1.82]', 'fmt/[^9.1]', 'spdlog/[^1.11]', 'date/[^3.0]', 'zlib/[^1.2]'
-    test_requires = 'gtest/cci.20210126'
-    generators = 'CMakeDeps', 'CMakeToolchain'
+    author = "Marko Malenic (mmalenic1@gmail.com)"
+    # x-release-please-start-version
+    version = "0.1"
+    # x-release-please-end
+
+    requires = 'boost/[^1]', 'spdlog/[^1]', 'date/[^3]', 'nlohmann_json/[^3]', 'cryptopp/[^8]', 'sqlitecpp/[^3]'
+    test_requires = "gtest/[^1]"
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
     options = {
-        'build_tests': [True, False],
-        'download_dependencies': [True, False],
-        'shared': [True, False]
+        # Build the binary CLI.
+        "build_bin": [True, False],
+        # Whether to build test executables.
+        "build_testing": [True, False],
+        # Whether to run clang tidy.
+        "run_clang_tidy": [True, False],
+        # An optional clang tidy executable.
+        "clang_tidy_executable": [None, "ANY"],
+        # Specifies a compiler launcher, such as sccache, which sets CMAKE_<LANG>_COMPILER_LAUNCHER.
+        "compiler_launcher": [None, "ANY"],
+        # Add a cmake install command after for the binary target if `build_binary` is also true:
+        # https://cmake.org/cmake/help/latest/command/install.html
+        "install_bin": [True, False],
+        # Add a cmake install command after for the library target:
+        # https://cmake.org/cmake/help/latest/command/install.html
+        "install_lib": [True, False],
     }
     default_options = {
-        'build_tests': True,
-        'download_dependencies': True,
-        'shared': True
+        "build_bin": True,
+        "build_testing": False,
+        "run_clang_tidy": False,
+        "clang_tidy_executable": None,
+        "compiler_launcher": None,
+        "install_bin": True,
+        "install_lib": True,
     }
+
+    def validate(self):
+        if self.settings.compiler.cppstd:
+            check_min_cppstd(self, 23)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+
+        tc.variables["EVLIST_BUILD_BIN"] = self.options.build_bin
+        tc.variables["BUILD_TESTING"] = self.options.build_testing
+        tc.variables["EVLIST_RUN_CLANG_TIDY"] = self.options.run_clang_tidy
+        tc.variables["EVLIST_INSTALL_BIN"] = self.options.install_bin
+        tc.variables["EVLIST_INSTALL_LIB"] = self.options.install_lib
+
+        if self.options.clang_tidy_executable:
+            tc.variables["EVLIST_CLANG_TIDY_EXECUTABLE"] = (
+                self.options.clang_tidy_executable
+            )
+        if self.options.compiler_launcher:
+            tc.variables["CMAKE_C_COMPILER_LAUNCHER"] = self.options.compiler_launcher
+            tc.variables["CMAKE_CXX_COMPILER_LAUNCHER"] = self.options.compiler_launcher
+
+        tc.generate()
 
     def layout(self):
         cmake_layout(self)
 
     def package_info(self):
-        self.cpp_info.requires = ['boost::algorithm', 'boost::asio', 'boost::program_options']
+        self.cpp_info.requires = ['boost::algorithm', 'boost::asio', 'boost::uuid', 'boost::numeric-conversion']
---
+++ justfile
@@ -0,0 +1,80 @@
+import 'sanitizers.just'
+
+# Detect the conan profile for building.
+profile:
+    conan profile detect -e
+
+# Update the lock file.
+update:
+    conan lock create . --lockfile-clean
+
+# Build evlist.
+build build_type='Debug' $COMPILER_VERSION='' *build_options='': profile clean_cache
+    conan build . --build=* -s build_type={{ capitalize(build_type) }} -s compiler.cppstd=23 {{ build_options }}
+
+# Build with the clang profile
+build_clang build_type='Debug' $COMPILER_VERSION='19' *build_options='': \
+    (build build_type COMPILER_VERSION '-pr ./profiles/clang ' + build_options)
+
+# Build with the gcc profile
+build_gcc build_type='Debug' $COMPILER_VERSION='14' *build_options='': \
+    (build build_type COMPILER_VERSION '-pr ./profiles/gcc ' + build_options)
+
+# Rebuild evlist using the existing CMake directory.
+rebuild build_type='Debug':
+    cd build/{{ capitalize(build_type) }} && cmake --build .
+
+# Build only the evlist library.
+build_lib build_type='Debug' $COMPILER_VERSION='' *build_options='': \
+    (build build_type COMPILER_VERSION '-o "&:build_bin=False" ' + build_options)
+
+# Build and install evlist.
+install build_type='Release' $COMPILER_VERSION='' *build_options='': \
+    (build build_type COMPILER_VERSION build_options)
+    cmake --install build/{{ capitalize(build_type) }}
+
+# Build and package the binary in a compressed file.
+package build_type='Release' name='evlist.tar.gz' $COMPILER_VERSION='' *build_options='': \
+    (build build_type COMPILER_VERSION build_options)
+    cd build/{{ capitalize(build_type) }} && tar -czf {{ name }} evlist && sha256sum -b {{ name }} > {{ name }}.sha256
+
+# Build and run evlist.
+run args='--help' build_type='Release' $COMPILER_VERSION='' *build_options='': \
+    (build build_type COMPILER_VERSION build_options)
+    cd build/{{ capitalize(build_type) }} && ./evlist {{ args }}
+
+_run_tests filter='*':
+    cd build/Debug && ./evlisttest --gtest_filter={{ filter }}
+
+# Build and test evlist.
+test filter='*' $COMPILER_VERSION='' *build_options='': \
+    (build 'Debug' COMPILER_VERSION '-o "&:build_testing=True" ' + build_options) (_run_tests filter)
+
+# Build and test evlist using the clang profile.
+test_clang filter='*' $COMPILER_VERSION='19' *build_options='': \
+    (build_clang 'Debug' COMPILER_VERSION '-o "&:build_testing=True" ' + build_options) (_run_tests filter)
+
+# Build and test evlist using the gcc profile.
+test_gcc filter='*' $COMPILER_VERSION='14' *build_options='': \
+    (build_gcc 'Debug' COMPILER_VERSION '-o "&:build_testing=True" ' + build_options) (_run_tests filter)
+
+# Run pre-commit and other lints.
+lint:
+    pre-commit run --all-files
+
+# Run clang tidy on code.
+check $COMPILER_VERSION='' *build_options='': \
+    lint (build 'Debug' COMPILER_VERSION '-o "&:build_testing=True" -o "&:run_clang_tidy=True" ' + build_options)
+
+# Remove the build directory.
+clean:
+    rm -rf build
+
+# Clean the CMakeCache.txt only.
+clean_cache:
+    rm -f build/Debug/CMakeCache.txt && rm -f build/Release/CMakeCache.txt
+
+# Generate documentation.
+doc:
+    git clone --depth 1 --branch v2.3.4 https://github.com/jothepro/doxygen-awesome-css.git build/doxygen-awesome-css || true
+    doxygen
