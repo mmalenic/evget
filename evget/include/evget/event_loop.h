23c23155e4d8278f4569f2dd9c555f0ad25e4a6e
mmalenic <mmalenic1@gmail.com>
2025-12-25 20:17:30 +1100

    refactor(evget): move event loop to concrete class

--- evget/include/evget/event_loop.h
+++ evget/include/evget/event_loop.h
@@ -1,44 +1,89 @@
 /**
  * \file event_loop.h
- * \brief Abstract event loop interface for processing input events.
+ * \brief The event loop for processing input events.
  */
 
 #ifndef EVGET_EVENT_LOOP_H
 #define EVGET_EVENT_LOOP_H
 
+#include <memory>
+
 #include "evget/event_listener.h"
+#include "evget/next_event.h"
 
 namespace evget {
-
 /**
  * \brief Class represents processing events.
  * \tparam T type of events to process
  */
 template <typename T>
-class EventLoop : public EventListener<T> {
+class EventLoop {
 public:
     /**
-     * \brief Register listeners to notify.
-     * \param event_listener event listener to register for notifications
+     * \brief Create a new event loop.
+     * \param next_event next event interface
+     * \param listeners the listeners to notify
      */
-    virtual void RegisterEventListener(EventListener<T>& event_listener) = 0;
+    EventLoop(NextEvent<T>& next_event, std::vector<std::reference_wrapper<evget::EventListener<T>>> listeners);
+
+    /**
+     * \brief Start processing events and notify listeners.
+     */
+    boost::asio::awaitable<evget::Result<void>> Start();
 
     /**
      * \brief Stop the event loop.
      */
-    virtual void Stop() = 0;
+    void Stop();
 
-    EventLoop() = default;
+    /**
+     * \brief Check if the event loop has been stopped.
+     * \return awaitable boolean indicating if the loop is stopped
+     */
+    boost::asio::awaitable<bool> IsStopped();
 
-    ~EventLoop() override = default;
+private:
+    std::reference_wrapper<NextEvent<T>> next_event_;
+    std::vector<std::reference_wrapper<evget::EventListener<T>>> listeners_;
 
-    EventLoop(EventLoop&&) noexcept = delete;
-    EventLoop& operator=(EventLoop&&) noexcept = delete;
-
-    EventLoop(const EventLoop&) = delete;
-    EventLoop& operator=(const EventLoop&) = delete;
+    bool stopped_{false};
 };
-
 } // namespace evget
+
+template <typename T>
+evget::EventLoop<T>::EventLoop(
+    NextEvent<T>& next_event,
+    std::vector<std::reference_wrapper<EventListener<T>>> listeners
+)
+    : next_event_{next_event}, listeners_{std::move(listeners)} {}
+
+template <typename T>
+boost::asio::awaitable<evget::Result<void>> evget::EventLoop<T>::Start() {
+    while (!co_await IsStopped()) {
+        for (auto& listener : listeners_) {
+            auto event = co_await next_event_.get().Next();
+            if (!event.has_value()) {
+                co_return Err{event.error()};
+            }
+
+            auto result = co_await listener.get().Notify(std::move(*event));
+            if (!result.has_value()) {
+                co_return Err{result.error()};
+            }
+        }
+    }
+
+    co_return evget::Result<void>{};
+}
+
+template <typename T>
+void evget::EventLoop<T>::Stop() {
+    stopped_ = true;
+}
+
+template <typename T>
+boost::asio::awaitable<bool> evget::EventLoop<T>::IsStopped() {
+    co_return stopped_;
+}
 
 #endif
