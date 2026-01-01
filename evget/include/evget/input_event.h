3f2a16f349d44e575058cd98fa2bafd800091aa0
mmalenic <mmalenic1@gmail.com>
2026-01-02 08:58:12 +1100

    feat(evget): move input event to main evget lib

---
+++ evget/include/evget/input_event.h
@@ -0,0 +1,58 @@
+/**
+ * \file input_event.h
+ * \brief Input event wrapper with timestamp measurement and data access.
+ */
+
+#ifndef EVGET_INPUT_EVENT_H
+#define EVGET_INPUT_EVENT_H
+
+#include "event/schema.h"
+
+namespace evget {
+
+/**
+ * \brief Represents an input event with a timestamp and event data.
+ * \tparam T the inner event data type
+ */
+template <typename T>
+class InputEvent {
+public:
+    /**
+     * \brief Create an input event.
+     * @param event inner event
+     */
+    explicit InputEvent(T event);
+
+    /**
+     * \brief Get the timestamp of when the event occurred.
+     * \return reference to the event timestamp
+     */
+    [[nodiscard]] const TimestampType& GetTimestamp() const;
+
+    /**
+     * \brief Get a reference to the event data.
+     * \return reference to the event data
+     */
+    [[nodiscard]] const T& ViewData() const;
+
+private:
+    T event_;
+    TimestampType timestamp_{Now()};
+};
+
+} // namespace evget
+
+template <typename T>
+evget::InputEvent<T>::InputEvent(T event) : event_{event} {}
+
+template <typename T>
+const evget::TimestampType& evget::InputEvent<T>::GetTimestamp() const {
+    return timestamp_;
+}
+
+template <typename T>
+const T& evget::InputEvent<T>::ViewData() const {
+    return event_;
+}
+
+#endif // EVGET_INPUT_EVENT_H
