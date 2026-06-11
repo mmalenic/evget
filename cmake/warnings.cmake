eaef31394f34942d9e7d2f88ecf22dd1a1614241
Marko Malenic <mmalenic1@gmail.com>
2026-06-11 14:18:52 +1000

    refactor: move clang tidy block to warnings, and apply per-target

--- cmake/warnings.cmake
+++ cmake/warnings.cmake
@@ -7,6 +7,19 @@ function(evget_apply_warnings target)
                           $<$<CXX_COMPILER_ID:MSVC>:/W4;/permissive->
     )
 
+    # Clang-tidy static analysis.
+    if(EVGET_RUN_CLANG_TIDY)
+        if(DEFINED EVGET_CLANG_TIDY_EXECUTABLE)
+            set(clang_tidy "${EVGET_CLANG_TIDY_EXECUTABLE}")
+        else()
+            set(clang_tidy "clang-tidy")
+        endif()
+        if(EVGET_CLANG_TIDY_FIX_ERRORS)
+            list(APPEND clang_tidy "--fix-errors")
+        endif()
+        set_target_properties(${target} PROPERTIES CXX_CLANG_TIDY "${clang_tidy}")
+    endif()
+
     # MSVC static analysis with /analyze.
     if(EVGET_RUN_MSVC_ANALYZE)
         target_compile_options(${target} PRIVATE $<$<CXX_COMPILER_ID:MSVC>:/analyze;/WX>)
